327242134482952a524ade7000d751921083d844
Robert Williams <robert@a3marketing.com>
2020-06-24 14:33:40 -0500

    set initial state and created function to search employees

--- src/App.js
+++ src/App.js
@@ -5,7 +5,22 @@ import Table from "./components/Table";
 import Header from "./components/Header";
 import API from "./utils/api";
 
+class App extends React.Component {
+  state = {
+    employees: [],
+    search: "",
+    sortID: false,
+    sortName: false,
+    sortEmail: false,
+    sortPhone: false
+  };
 
+  searchEmployee = () => {
+    API.search()
+    .then(res => this.setState({employees: res.data}))
+    .catch(err => console.log(err));
+  }
+}
 
 
 export default App;
